037113ad9915e68ef238b457e19070df9eea9079
David Ford <david@blue-labs.org>
2018-02-12 06:10:00 -0500

    trim fluff

--- App-Android/app/src/main/java/org/fireground/dispatchbuddy/dispatchbuddy/LoginActivity.java
+++ App-Android/app/src/main/java/org/fireground/dispatchbuddy/dispatchbuddy/LoginActivity.java
@@ -9,9 +9,6 @@ import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ProgressBar;
-import android.widget.RelativeLayout;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
