d2c15f4d673a5ecd9e5ff5c1e17635a20681b1ff
David Ford <david@blue-labs.org>
2018-02-24 21:56:03 -0500

    JSONObjectRequests ... because com.android gmap request is really awful

---
+++ App-Android/app/src/main/java/org/fireground/dispatchbuddy/JsonObjectCallbackInterface.java
@@ -0,0 +1,11 @@
+package org.fireground.dispatchbuddy;
+
+import org.json.JSONObject;
+
+/**
+ * Created by david on 2/22/18.
+ */
+
+public interface JsonObjectCallbackInterface {
+    void onSuccess(JSONObject result);
+}
