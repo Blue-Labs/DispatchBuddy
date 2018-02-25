3e62777f42cd9b8f460f65e3d2b6ff46c9a6089b
David Ford <david@blue-labs.org>
2018-02-24 21:55:24 -0500

    ...and the responders adapter

---
+++ App-Android/app/src/main/java/org/fireground/dispatchbuddy/DispatchRespondersAdapter.java
@@ -0,0 +1,83 @@
+package org.fireground.dispatchbuddy;
+
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.text.ParseException;
+import java.util.List;
+
+/**
+ * Created by david on 2/24/18.
+ */
+
+public class DispatchRespondersAdapter extends RecyclerView.Adapter<DispatchRespondersAdapter.DispatchRespondersViewHolder> {
+
+    final private String TAG = "DA";
+    final private DispatchBuddyBase DBB = DispatchBuddyBase.getInstance();
+
+    // todo: make a POJO dispatchRespondersModel;
+    private List<String> list;
+    CustomItemClickListener listener;
+
+    public DispatchRespondersAdapter(List<String> list, CustomItemClickListener listener) {
+        this.list = list;
+        this.listener = listener;
+    }
+
+    @Override
+    public int getItemCount() {
+        return list.size();
+    }
+
+    @Override
+    public void onBindViewHolder(DispatchRespondersViewHolder holder, int position) {
+        String person = list.get(position);
+    }
+
+    @Override
+    public DispatchRespondersViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.dispatch_item, parent, false);
+
+        final DispatchRespondersViewHolder mViewHolder = new DispatchRespondersViewHolder(view);
+
+        view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.e(TAG, "onClick fired for: "+v.getId());
+                listener.onItemClick(v, mViewHolder.getLayoutPosition());
+            }
+        });
+
+        view.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                return listener.onItemLongClick(v, mViewHolder.getLayoutPosition());
+            }
+        });
+        return mViewHolder;
+    }
+
+    public static class DispatchRespondersViewHolder extends RecyclerView.ViewHolder {
+        ImageView profileIcon;
+        TextView responderName;
+        ImageButton locateButton;
+
+        View item;
+
+        public DispatchRespondersViewHolder(View itemView) {
+            super(itemView);
+            item = itemView;
+            profileIcon = (ImageView) itemView.findViewById(R.id.profileIcon);
+            responderName = (TextView) itemView.findViewById(R.id.responderName);
+            locateButton = (ImageButton) itemView.findViewById(R.id.locatePerson);
+        }
+    }
+
+}
